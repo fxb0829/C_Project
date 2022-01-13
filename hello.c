048abf69a9a8130a6f3a2c9aabcda80784ec83a7
fxb <931935931@qq.com>
2022-01-13 12:33:57 +0800

    ceshi

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Author: fengxb
+ * @Date: 2022-01-13 12:31:51
+ * @LastEditor: fengxb
+ * @LastEditTime: 2022-01-13 12:31:51
+ */
+
+#include <stdio.h>
+
+int main(){
+
+    printf("hello world!\n");
+    return -1;
+}
